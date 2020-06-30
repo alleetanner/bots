9b9e4bfd580ea256ea7102ea71f1fab08deab13d
Al Tanner <8190834+altanner@users.noreply.github.com>
2020-07-01 00:47:15 +0100

    Update bozbot.sh

--- bozbot/bozbot.sh
+++ bozbot/bozbot.sh
@@ -3,19 +3,19 @@
 # post if interrupted in background
 trap interrupt 1 3 6 15
 function interrupt() {
-    curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: BozBot down, SIGINT interrupt."}' https://YOUR_SLACK_WEBHOOK
+    curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: BozBot down, SIGINT interrupt."}' https://YOUR_SLACK_WEBHOOK
     exit 0;
 }
 
 # post if by ctrl-c
 trap ctrl-c SIGINT
 function ctrl-c() {
-    curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: BozBot down, ctrl-c interrupt."}' https://YOUR_SLACK_WEBHOOK
+    curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: BozBot down, ctrl-c interrupt."}' https://YOUR_SLACK_WEBHOOK
     exit 0;
 }
 
 # post that bozbot is up
-curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: BozBot up."}' https://YOUR_SLACK_WEBHOOK
+curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: BozBot up."}' https://YOUR_SLACK_WEBHOOK
 
 # set booleans, toggled to stop repeated posts when nothing has changed.
 running=0;
@@ -27,23 +27,23 @@ do
         if [ `pgrep epicosm_ | wc -l` == 0 ]
             then
                 # catch if goes down while harvesting
-                curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm looks down, interrupted while active."}' https://YOUR_SLACK_WEBHOOK
+                curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm looks down, interrupted while active."}' https://YOUR_SLACK_WEBHOOK
                 up=0;
             else
                 # catch when harvest finished
-                curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm harvest finished, waiting for next harvest."}' https://YOUR_SLACK_WEBHOOK
+                curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm harvest finished, waiting for next harvest."}' https://YOUR_SLACK_WEBHOOK
         fi
         running=0;
     elif [ `pgrep mongod | wc -l` != 0 ] && [ $running == 0 ]
         then
             # catch harvest start
-            curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm harvest started."}' https://YOUR_SLACK_WEBHOOK
+            curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm harvest started."}' https://YOUR_SLACK_WEBHOOK
             running=1;
             up=1;
     elif [ `pgrep epicosm_ | wc -l` == 0 ] && [ $running == 0 ] && [ $up == 1 ]
         then
             # catch if goes down while NOT harvesting
-            curl -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm looks down, interrupted while idle."}' https://YOUR_SLACK_WEBHOOK
+            curl -s -X POST -H 'Content-type: applicati/json' --data '{"text":"IEUVM: epicosm looks down, interrupted while idle."}' https://YOUR_SLACK_WEBHOOK
         up=0;
     fi
     sleep 60; # check status once a minute
